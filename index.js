723c105fe17b326c153cddee1d7624005d54dd3b
Ugo <s_per00@outlook.com>
2020-07-29 22:08:34 +0300

    able to play sampled sounds need reafctoring

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
+//everything needs to be refactored classbased
+
+//fetching audionfile, decoding and return buffer
 async function getFile(audioCtx, filepath) {
     const res = await fetch(filepath);
     const arrayBuff = await res.arrayBuffer();
     const audioBuff = await audioCtx.decodeAudioData(arrayBuff);
     return audioBuff;
 }
-
+//need change for multiple files
 async function setupSample(audioCtx) {
     const filePath = './audioFiles/HAT.wav';
     const sample = await getFile(audioCtx, filePath);
     return sample
 
 }
-
+//need to add gain and other modifications
 function playSample(audioCtx, audioBuff) {
     const sampleSource = audioCtx.createBufferSource();
     sampleSource.buffer = audioBuff;
@@ -27,6 +30,7 @@ const audioCtx = new AudioContext();
 
 
 function playHandler() {
+    console.log('run')
     if(audioCtx.state !== 'running') {
         audioCtx.resume();
     }
